3cdb129a5454142f04c1986ec4984832514403c2
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-17 11:42:48 +0530

    setupped native wind , tailwind and react navigation

---
+++ components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View } from "react-native";
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-400">
+      <Text className="text-2xl">Tailwind Setup + Navigation</Text>
+    </View>
+  );
+}
