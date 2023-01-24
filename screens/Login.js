9da40097232166e3373b5f61556159365f97fe1e
arnab <arnabchatterjee.ac.2@gmail.com>
2023-01-24 19:32:03 +0530

    added setting token on successful login

--- screens/Login.js
+++ screens/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSignIn } from "../features/authSlice";
 import { getInfo } from "../features/inputSlice";
-import { handleFetchError, loginUser } from "../utils/utils";
+import { handleFetchError, loginUser, setToken } from "../utils/utils";
 import Form from "./Form";
 
 export default function Login() {
@@ -11,6 +11,7 @@ export default function Login() {
   const getUser = async () => {
     try {
       const token = await loginUser(userInfo);
+      await setToken(token)
       dispatch(setSignIn({ token }));
     } catch (error) {
       const errorCode = parseInt(error.message);
